e11dea7224792b43031fa1900e292647a8103a9e
Yogendra Sahu <workspace.yogisahu01@gmail.com>
2026-06-13 11:58:32 +0530

    Home Controller Changes

--- BasketIQ.APP/src/main.ts
+++ BasketIQ.APP/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { App } from './app/app';
-
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app';
+ 
+bootstrapApplication(AppComponent, appConfig)
+  .catch(err => console.error(err));
